c94bccfbc850399bcb893eb3a545661392759f43
pauldzy <paul@dziemiela.com>
2018-11-29 20:54:55 -0500

    change order

--- geoserver/init.sh
+++ geoserver/init.sh
@@ -6,15 +6,17 @@ url="http://localhost:8080/geoserver/rest/"
 
 for i in {1..20}
 do
-   curl -u admin:geoserver -X GET -H "$aj" -H "$cj" ${url}layers
+   curl -u admin:geoserver --fail -X GET -H "$aj" -H "$cj" ${url}layers
    if [ $? -eq 0 ]
    then
       curl -u admin:geoserver -X PUT -H "$aj" -H "$cj" -d '{"newPassword":"nhdplus"}' ${url}security/self/password
+      sleep 2
+      
+      curl -u admin:nhdplus -X PUT -H "$aj" -H "$cj" -d '{"addressCity":"","addressCountry":"","addressType":"","contactEmail":"","contactOrganization":"NHDPlusInABox","contactPerson":"","contactPosition":""}' ${url}settings/contact
+      sleep 2
       
       curl -u admin:nhdplus -X PUT -H "$aj" -H "$cj" -d '{"oldMasterPassword":"geoserver","newMasterPassword":"nhdplus123"}' ${url}security/masterpw
       
-      curl -u admin:nhdplus -X PUT -H "$aj" -H "$cj" -d '{"addressCity":"","addressCountry":"","addressType":"","contactEmail":"","contactOrganization":"NHDPlusInABox","contactPerson":"","contactPosition":""}' ${url}settings/contact
-   
       exit 0
       
    fi
