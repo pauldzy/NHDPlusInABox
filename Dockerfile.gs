1d766caf3f90f57b5b4d71d281248d4ab30c7df4
Paul Dziemiela <paul@dziemiela.com>
2018-11-30 18:23:18 -0500

    Minor fixes

--- Dockerfile.gs
+++ Dockerfile.gs
@@ -14,6 +14,7 @@ RUN apt-get update                             &&\
     apt-get install -y --no-install-recommends   \
        supervisor                                \
        dos2unix                                  \
+       jq                                        \
        curl                                    &&\
     rm -rf /var/lib/apt/lists/*
 
--- geoserver/init.sh
+++ geoserver/init.sh
@@ -3,27 +3,132 @@
  aj="Accept: application/json"
  cj="Content-type: application/json"
 url="http://localhost:8080/geoserver/rest/"
+pwd=0
+rc=0
 
-for i in {1..20}
+for i in {1..500}
 do
-   curl -u admin:geoserver --fail -X GET -H "$aj" -H "$cj" ${url}layers
-   if [ $? -eq 0 ]
+   echo $i
+   curl -u admin:geoserver --fail -X GET -H "$aj" -H "$cj" ${url}layers 1> /dev/null
+   ret=$?
+   
+   if [ $ret -eq 0 ]
    then
-      curl -u admin:geoserver -X PUT -H "$aj" -H "$cj" -d '{"newPassword":"nhdplus"}' ${url}security/self/password
+      echo "server now responding"
+      break
+         
+   else
+      curl -u admin:nhdplus --fail -X GET -H "$aj" -H "$cj" ${url}layers 1> /dev/null
+      ret2=$?
+      
+      if [ $ret2 -eq 0 ]
+      then
+         pwd=1
+         echo "server now responding"
+         break
+         
+      fi
+      
+   fi
+   
+   if [ $i -gt 499 ]
+   then
+      echo "server failed to respond"
+      exit -99
+      
+   fi
+   
+   sleep 2
+
+done
+
+if [ $pwd -eq 0 ]
+then
+   for i in {1..100}
+   do
+      echo $i
+      curl -u admin:geoserver --fail -X PUT -H "$aj" -H "$cj" -d '{"newPassword":"nhdplus"}' ${url}security/self/password
+      ret=$?
+      
+      if [ $ret -eq 0 ]
+      then
+         echo "changing admin password"
+         break
+         
+      fi
+      
+      if [ $i -gt 99 ]
+      then
+         echo "unable to change admin password"
+         rc=1
+         
+      fi
+      
       sleep 2
       
-      curl -u admin:nhdplus -X PUT -H "$aj" -H "$cj" -d '{"contact":{"addressCity":"","addressCountry":"","addressType":"","contactEmail":"","contactOrganization":"NHDPlusInABox","contactPerson":"","contactPosition":""}}' ${url}settings/contact
-      sleep 2
+   done
+
+else
+   echo "password already changed"
+   
+fi
+  
+for i in {1..100}
+do
+   echo $i
+   curl -u admin:nhdplus --fail -X PUT -H "$aj" -H "$cj" -d '{"contact":{"addressCity":"","addressCountry":"","addressType":"","contactEmail":"","contactOrganization":"NHDPlusInABox","contactPerson":"","contactPosition":""}}' ${url}settings/contact
+   ret=$?
       
-      curl -u admin:nhdplus -X PUT -H "$aj" -H "$cj" -d '{"oldMasterPassword":"geoserver","newMasterPassword":"nhdplus123"}' ${url}security/masterpw
+   if [ $ret -eq 0 ]
+   then
+      echo "changing server contact info"
+      break
       
-      exit 0
+   fi
       
+   if [ $i -gt 99 ]
+   then
+      echo "unable to change contact info"
+      rc=1
+   fi
+   
+   sleep 2
+   
+done
+  
+for i in {1..100}
+do
+   echo $i
+   
+   pw=$(curl -u admin:nhdplus --fail -X GET -H "$aj" -H "$cj" ${url}security/masterpw | jq -r .oldMasterPassword)
+   
+   if [ $pw = "nhdplus123" ]
+   then
+      echo "master password already changed"
+      break
+      
+   fi
+   
+   curl -u admin:nhdplus --fail -X PUT -H "$aj" -H "$cj" -d '{"oldMasterPassword":"geoserver","newMasterPassword":"nhdplus123"}' ${url}security/masterpw
+   ret=$?
+      
+   if [ $ret -eq 0 ]
+   then
+      echo "changing master password"
+      break
+      
+   fi
+      
+   if [ $i -gt 99 ]
+   then
+      echo "unable to change master password"
+      rc=1
+   
    fi
    
    sleep 2
    
 done
 
-exit -99
+exit $rc
 
